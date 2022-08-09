4514480e1d60e313052aed7244f4522c88be122e
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-09 19:18:18 +0300

    gameview background color updated

--- lib/view/game_view.dart
+++ lib/view/game_view.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/gradient_background.dart';
+
 class GameView extends StatelessWidget {
   const GameView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Game View'),
+    return Scaffold(
+        body: GradientBackground(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Theme.of(context).colorScheme.primary,
+          Theme.of(context).secondaryHeaderColor
+        ],
       ),
-    );
+      child: const Center(
+        child: Text('Game view'),
+      ),
+    ));
   }
 }
