111906acaa09d017d60255743ecffccbda000cb8
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-09 17:04:33 +0300

    added app_assets

---
+++ lib/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAsset {
+  static String xWithShadow = 'lib/asset/icon/x_shadow.svg';
+  static String xWithUnShadow = 'lib/asset/icon/x_unshadow.svg';
+  static String oWithShadow = 'lib/asset/icon/o_shadow.svg';
+  static String oWithUnShadow = 'lib/asset/icon/o_unshadow.svg';
+
+  static String oVector = 'lib/asset/icon/o_vector.svg';
+  static String xVector = 'lib/asset/icon/x_vector.svg';
+}
