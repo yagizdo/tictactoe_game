3a42c95a8caa1cfb0d3f62ee3049ad4910b683de
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-11 18:51:07 +0300

    added event on gridbox

--- lib/widgets/game_view/grid_box.dart
+++ lib/widgets/game_view/grid_box.dart
@@ -1,33 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:simple_shadow/simple_shadow.dart';
+import 'package:tictactoe_app/constants/app_assets.dart';
 import 'package:tictactoe_app/constants/app_theme.dart';
 
+import '../../bloc/backend_bloc.dart';
+
 class GridBox extends StatelessWidget {
-  const GridBox({Key? key, required this.value}) : super(key: key);
-  final String value;
+  const GridBox({Key? key, required this.index}) : super(key: key);
+  final int index;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.w),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(20),
-          ),
+      child: InkWell(
+        onTap: () {
+          BlocProvider.of<BackendBloc>(context).add(ClickEvent(index));
+        },
+        child: BlocBuilder<BackendBloc, BackendState>(
+          builder: (context, state) {
+            if (state is GameState) {
+              var value = state.gameList[index];
+              return Container(
+                decoration: const BoxDecoration(
+                  color: white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: Center(
+                  child: value.isEmpty
+                      ? const SizedBox()
+                      : SimpleShadow(
+                          color: value.contains('X') ? lightBlue : Colors.red,
+                          child: SvgPicture.asset(
+                            value.contains('X')
+                                ? AppAsset.xWithUnShadow
+                                : AppAsset.oWithUnShadow,
+                            width: 60.w,
+                          ),
+                        ),
+                ),
+              );
+            }
+
+            return Container(
+              decoration: const BoxDecoration(
+                color: white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: const Center(
+                child: SizedBox(),
+              ),
+            );
+          },
         ),
-        child: Center(
-            child: value.isEmpty
-                ? const SizedBox()
-                : SimpleShadow(
-                    color: value.contains('x') ? lightBlue : Colors.red,
-                    child: SvgPicture.asset(
-                      value,
-                      width: 60.w,
-                    ),
-                  )),
       ),
     );
   }
